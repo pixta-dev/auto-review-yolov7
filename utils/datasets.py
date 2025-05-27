a67318df172e95bc6a3ff5fbe4c660fb51683fea
MinhHuuNguyen <minhhuunguyen1511@gmail.com>
2025-05-27 10:00:47 +0000

    Revert "feat: :sparkles: letterbox return square image"
    This reverts commit 2c4b6fa3073f5180bc263338b15070729aa68782.

--- utils/datasets.py
+++ utils/datasets.py
@@ -981,7 +981,7 @@ def replicate(img, labels):
     return img, labels
 
 
-def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32, return_square_img=False):
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
     # Resize and pad image while meeting stride-multiple constraints
     shape = img.shape[:2]  # current shape [height, width]
     if isinstance(new_shape, int):
@@ -1002,20 +1002,12 @@ def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scale
         dw, dh = 0.0, 0.0
         new_unpad = (new_shape[1], new_shape[0])
         ratio = new_shape[1] / shape[1], new_shape[0] / shape[0]  # width, height ratios
-    
-    if return_square_img:
-        diff = abs(new_unpad[0] - new_unpad[1] + dw - dh)
-        if new_unpad[0] > new_unpad[1]:
-            dh += diff
-        else:
-            dw += diff
 
     dw /= 2  # divide padding into 2 sides
     dh /= 2
 
     if shape[::-1] != new_unpad:  # resize
         img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
-        
     top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
     left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
     img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
